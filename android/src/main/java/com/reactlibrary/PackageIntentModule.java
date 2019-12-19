a0ce01c4562df8bb3141683a6cd67bb8adf45012
Conor Fayle <conor.fayle@gmail.com>
2019-12-19 13:00:45 +0000

    Added getAppList method

--- android/src/main/java/com/reactlibrary/PackageIntentModule.java
+++ android/src/main/java/com/reactlibrary/PackageIntentModule.java
@@ -3,11 +3,16 @@ package com.reactlibrary;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
 
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Collections;
+import java.util.List;
 
 public class PackageIntentModule extends ReactContextBaseJavaModule {
 
@@ -27,6 +32,24 @@ public class PackageIntentModule extends ReactContextBaseJavaModule {
     public void canStartIntent(String packageName, Callback callback) {
         Intent intent = getIntent(packageName);
         callback.invoke(intent != null);
+    }
+
+    @ReactMethod
+    public List getAppList() {
+        PackageManager pm = getReactApplicationContext().getPackageManager();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> appList = pm.queryIntentActivities(mainIntent, 0);
+        Collections.sort(appList, new ResolveInfo.DisplayNameComparator(pm));
+
+        for (ResolveInfo rInfo : appList) {
+            Log.d("Launcher", "package:" + rInfo.activityInfo.packageName +
+                    "; activity: " + rInfo.activityInfo.name);
+        }
+
+        return appList;
     }
 
     private Intent getIntent(String packageName) {
